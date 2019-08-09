f9679b4bb535d06afc5868fb2dcf1ca3c218262a
jecikak <jelenakaraulic992@gmail.com>
2019-08-09 10:53:34 +0200

    update files connection

--- task1/db.php
+++ task1/db.php
@@ -1,50 +1,37 @@
 <?php 
+include "dbconst.php";
+class DB {
 
-//connection database - pdo
-/*try{
-	$connector= new PDO('mysql:dbname=schooldiary;host=localhost','root','');
-    $connector->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);	
-	
-}catch(PDOExeption $e){
-	echo $e->getMessage();
-	die();
-}*/
-//print_r(PDO::getAvailableDrivers());
+   
+    public static $con;
 
-class DB{
-
-    private $host = '';
-    private $dbname = '';
-    private $username = '';
-    private $password ='';
-
-    
-
-    function __construct($host, $dbname, $username, $password){
-
-        $this->host = $host;
-        $this->dbname = $dbname;
-        $this->username = $username;
-        $this->password = $password;
+    public function __construct(){
         $this->connect();
-
     }
 
-    function connect(){
+    public function connect(){
 
         try{
 
-            $this->con = new PDO("mysql:host=$this->host;dbname=$this->dbname",$this->username, $this->password);
-            $this->con->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+            self::$con = new PDO('mysql: host=localhost; dbname=schooldiary','root','');
+            self::$con->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 			//print_r(PDO::getAvailableDrivers());
         }catch(PDOException $e){
 			echo 'Database connection has failed. Contact system administrator to resolve this issue!<br>';
 			$e->getMessage();
 
         }
+
 	}
 }
-$connection = new DB('localhost','schooldiary','root','');
-$connection->connect();
 
-	
+
+
+
+  
+ 
+
+ 
+ 
+
+

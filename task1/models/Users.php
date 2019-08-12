0f5237aa6555a4617ef608fa04f678e2af060129
jelena_jess <petkovjelena@gmail.com>
2019-08-12 12:40:39 +0200

    cddddcs

--- task1/models/Users.php
+++ task1/models/Users.php
@@ -5,7 +5,7 @@ class Users
 
     public function login_user($username, $password)
     {
-        $query = 'select users.id, users.first_name, users.last_name, users.username, users.password, users.role_id, role.name as role_name from users join role where username = "'.$username.'" and password = "'.$password.'"';
+        $query = 'select users.id, users.first_name, users.last_name, users.username, users.password, users.role_id, role.name as role_name from users join role on users.role_id=role.id  where users.username = "'.$username.'" and users.password = "'.$password.'"';
         $res = DB::$conn->query($query);
         $users = [];
 		while ($user = $res->fetch(PDO::FETCH_ASSOC)) {

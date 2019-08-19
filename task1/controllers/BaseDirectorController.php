9735f31008e23b6c8ed8f9a3187b6a041c447802
jecikak <jelenakaraulic992@gmail.com>
2019-08-19 10:10:02 +0200

    1

--- task1/controllers/BaseDirectorController.php
+++ task1/controllers/BaseDirectorController.php
@@ -17,8 +17,8 @@ class BaseDirectorController
 	public function diary()
 	{
 		$view = new View();
-		$all_classes = Director::class();
-		$view->data['classes'] = $all_classes;
+		//$all_classes = Director::class();
+		//$view->data['classes'] = $all_classes;
 		$view->load_view('director', 'pages', 'welcome');
 
 	}

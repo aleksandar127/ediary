5775591c8c717bac2082d6a59954240e80a5f6f7
Marjan Despotovic <makidespotovic95@gmail.com>
2019-08-20 10:50:53 +0200

    no message

--- task1/controllers/BaseTeacherController.php
+++ task1/controllers/BaseTeacherController.php
@@ -1,12 +1,36 @@
 <?php 
 
-class BaseTeacherController 
-{
-
-    public function index()
-    {
-        echo "<br>";
-        var_dump('isidora je kraljica sveta');
-    }
+class BaseTeacherController{
+    public function __construct($demand){
+		$this->demand = $demand;
 	
+    }
+    
+	public function index(){
+        $view = new View();
+		$all_students = Teacher::get_all_students();
+        $view->data['students'] = $all_students;
+        $view->load_view('teacher', 'pages', 'home');
+    }
+
+    public function subjects(){
+        $view = new View();
+        $all_subjects = Teacher::get_all_subjects();
+        $view->data['subjects'] = $all_subjects;
+        $view->data['students'] = Teacher::get_all_students();
+        $view->load_view('teacher', 'pages', 'subjects');
+    }
+
+    public function parents(){
+        $view = new View();
+        $all_parents = Teacher::get_all_parents();
+        $view->data['parents'] = $all_parents;
+        $view->load_view('teacher', 'pages', 'chat');
+    }
+    public function objects(){
+        $view = new View();
+        $all_subjects = Teacher::get_all_subjects();
+        $view->data['subjects'] = $all_subjects;
+        $view->load_view('teacher', 'pages', 'objects');
+    }
 }
---
+++ task1/views/teacher/pages/chat.php
@@ -0,0 +1,86 @@
+    <div id="message">
+        <div class="wrapper">
+            <div id="sendMessage">
+                <div id="scroll">
+                    <div class="container">
+                        <p>Hello. How are you today?</p>
+                        <span class="time-right">11:00</span>
+                    </div>
+                    <div class="container darker">
+                        <p>Hey! I'm fine. Thanks for asking!</p>
+                        <span class="time-left">11:01</span>
+                    </div>
+                    <div class="container">
+                        <p>Sweet! So, what do you wanna do today?</p>
+                        <span class="time-right">11:02</span>
+                    </div>
+                    <div class="container darker">
+                        <p>Nah, I dunno. Play soccer.. or learn more coding perhaps?</p>
+                        <span class="time-left">11:05</span>
+                    </div>
+                    <div class="container">
+                        <p>Sweet! So, what do you wanna do today?</p>
+                        <span class="time-right">11:02</span>
+                    </div>
+                    <div class="container darker">
+                        <p>Nah, I dunno. Play soccer.. or learn more coding perhaps?</p>
+                        <span class="time-left">11:05</span>
+                    </div>
+                    <div class="container">
+                        <p>Sweet! So, what do you wanna do today?</p>
+                        <span class="time-right">11:02</span>
+                    </div>
+                    <div class="container darker">
+                        <p>Nah, I dunno. Play soccer.. or learn more coding perhaps?</p>
+                        <span class="time-left">11:05</span>
+                    </div>
+                    <div class="container">
+                        <p>Sweet! So, what do you wanna do today?</p>
+                        <span class="time-right">11:02</span>
+                    </div>
+                    <div class="container darker">
+                        <p>Nah, I dunno. Play soccer.. or learn more coding perhaps?</p>
+                        <span class="time-left">11:05</span>
+                    </div>
+                    <div class="container">
+                        <p>Sweet! So, what do you wanna do today?</p>
+                        <span class="time-right">11:02</span>
+                    </div>
+                    <div class="container darker">
+                        <p>Nah, I dunno. Play soccer.. or learn more coding perhaps?</p>
+                        <span class="time-left">11:05</span>
+                    </div>
+                </div><!-- #scroll-->
+                <div id="chat">
+                    <form action="#">
+                        <textarea id="subject" name="subject" placeholder="Write something.." ></textarea>
+                        <input type="submit" value="Posalji">
+                    </form>
+                </div>
+            </div><!-- end #sendMessage -->
+            <div id="listUsers">
+                <ul>
+                    <?php foreach($this->data['parents'] as $parent): ?>
+                    <li><a href="#"><span><?php echo $parent['first_name']; ?></span> <span><?php echo $parent['last_name']; ?></span>  - Nikola</a></li>
+                    <?php endforeach; ?>
+                    <li><a href="#">Zoran Stankovic - Nikola</a></li>
+                    <li><a href="#">Zoran Stankovic - Nikola</a></li>
+                    <li><a href="#">Zoran Stankovic - Nikola</a></li>
+                    <li><a href="#">Zoran Stankovic - Nikola</a></li>
+                    <li><a href="#">Zoran Stankovic - Nikola</a></li>
+                    <li><a href="#">Zoran Stankovic - Nikola</a></li>
+                    <li><a href="#">Zoran Stankovic - Nikola</a></li>
+                    <li><a href="#">Zoran Stankovic - Nikola</a></li>
+                    <li><a href="#">Zoran Stankovic - Nikola</a></li>
+                    <li><a href="#">Zoran Stankovic - Nikola</a></li>
+                    <li><a href="#">Zoran Stankovic - Nikola</a></li>
+                    <li><a href="#">Zoran Stankovic - Nikola</a></li>
+                    <li><a href="#">Zoran Stankovic - Nikola</a></li>
+                    <li><a href="#">Zoran Stankovic - Nikola</a></li>
+                    <li><a href="#">Zoran Stankovic - Nikola</a></li>
+                    <li><a href="#">Zoran Stankovic - Nikola</a></li>
+                    <li><a href="#">Zoran Stankovic - Nikola</a></li>
+                </ul>
+            </div><!-- end #listUsers -->
+        </div><!-- end .wrapper -->
+    </div><!-- end #message -->

404985474a8dbcb74052ccbd2903231f95e50625
alex8716 <aleksandarmarkoviccrni@gmail.com>
2019-08-25 16:38:01 +0200

    no message

--- task1/controllers/BaseParentController.php
+++ task1/controllers/BaseParentController.php
@@ -2,5 +2,34 @@
 
 class BaseParentController 
 {
+
+    public function __construct($demand)
+    {
+		$this->demand = $demand;
+		
+	}
+	public function index()
+	{
+        $view = new View();
+        $grades = Parents::index(2);
+		$view->data['grades'] = $grades;	
+		$view->load_view('parent', 'pages', 'home');
+
+	}
+
+
+    public function logout()
+	{
+		$access_destroy = BaseAccessController::logout($_COOKIE['id'], $_COOKIE['loginhash']);
+		header('Location: http://localhost/eDiary/task1/');
+		die();
+		
+	}
+
+
+
+
+
+
 	
 }
---
+++ task1/views/parent/pages/home.php
@@ -0,0 +1,14 @@
+
+<h1>Roditelji</h2>
+
+<?php
+
+
+print_r($this->data['grades']);
+
+
+
+
+
+
+?>

2c9003d4fb86579fea91ee5b8c8018085f779179
alex8716 <aleksandarmarkoviccrni@gmail.com>
2019-08-26 14:37:00 +0200

    no message

--- task1/models/Parent.php
+++ task1/models/Parent.php
@@ -1,6 +1,6 @@
 <?php
 
-class Parents{
+class Parents{ 
 
     public static function index(){
         $query = DB::$conn->prepare('SELECT students.first_name,students.last_name,subjects.name,subjects_has_grades.grades from students join subjects_has_grades_has_students on subjects_has_grades_has_students.students_id=students.id join subjects_has_grades on subjects_has_grades.id=subjects_has_grades_has_students.subjects_has_grades_id join subjects on subjects.id=subjects_has_grades.subjects_id

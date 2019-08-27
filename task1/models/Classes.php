7f4b4c16dfba37f088b4560d69ca4859f5abfb30
Bagheera <andjelkamladenovic8@gmail.com>
2019-08-27 12:25:57 +0200

    jjj

--- task1/models/Classes.php
+++ task1/models/Classes.php
@@ -11,10 +11,10 @@ class Classes
         return $classes;
     }
 
-    public static function get_teachers_or_profs($high_low)
+    public static function get_teachers_or_profs($role_id)
     {
-        $query = DB::$conn->prepare('');
-        $query->execute([$high_low]); 
+        $query = DB::$conn->prepare('select users.first_name, users.last_name, class.high_low from class join users on class.users_id = users.id join role on users.roles_id = role.id where role.id = ?');
+        $query->execute([$role_id]); 
         $heads = $query->fetchAll(PDO::FETCH_ASSOC);
         return $heads;
     }

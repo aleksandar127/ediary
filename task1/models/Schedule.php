7d070260b83fd1b95c547938c2e4d056a7036549
aleksandar dmitrovic <admitrovic@yahoo.com>
2019-09-02 12:03:35 +0200

    no message

--- task1/models/Schedule.php
+++ task1/models/Schedule.php
@@ -10,6 +10,13 @@ public static function get_schedule_for_professor(){
     return $schedule;
    }
 
+   public static function get_schedule_for_teacher(){
+    $query = DB::$conn->prepare('select schedule.day_in_week,schedule.lesson_no,subjects.name from schedule join subjects on subjects.id=schedule.subjects_id  join class on class.id=schedule.class_id where class.users_id=?');
+    $query->execute([$_COOKIE['id']]);
+    $schedule = $query->fetchAll(PDO::FETCH_ASSOC);
+    return $schedule;
+   }
+
 
 
 }

419e87244f49e046c188f0597837d4f61705550e
isidora <isidoranikolic994@gmail.com>
2019-09-03 12:28:01 +0200

    pravljenje rasporeda

--- task1/views/admin/pages/make_sch.php
+++ task1/views/admin/pages/make_sch.php
@@ -1,105 +1,109 @@
 <div class="container">
     <form method="POST" action="#">
-    <!-- <div class="form-group">
-        <label for="class_sch">Odaberite razred za koji pravite raspored:</label>
-        <select class="form-control" id="class_sch" name="class_sch">
-        <?php foreach($this->data['available_classes'] as $avl_cls): ?>
-            <option value="<?php echo $avl_cls['id']; ?>"><?php echo $avl_cls['name']; ?></option>
-        <?php endforeach;?>
+        <div class="form-group">
+            <label for="class_sch">Odaberite razred za koji pravite raspored:</label>
+            <select class="form-control" id="class_sch" name="class_sch">
+                <option></option>   
+                <?php foreach($this->data['available_classes'] as $avl_cls): ?>
+                    <option value="<?php echo $avl_cls['id']; ?>"><?php echo $avl_cls['name']; ?></option>
+                <?php endforeach;?>
+            </select>
+        </div>
+    <div class="form-row">
+        <div class="col">
+            <div>Ponedeljak</div>
+            <input type="text" class="form-control">
+        </div>
+        <div class="col">
+            <div>Utorak</div>
+            <input type="text" class="form-control">
+        </div>
+        <div class="col">
+            <div>Sreda</div>
+            <input type="text" class="form-control">
+        </div>
+        <div class="col">
+            <div>Četvrtak</div>
+            <input type="text" class="form-control">
+        </div>
+        <div class="col">
+            <div>Petak</div>
+            <input type="text" class="form-control">
+        </div>
+    </div>
+    <div class="form-row">
+        <div class="col">
+            <input type="text" class="form-control">
+        </div>
+        <div class="col">
+            <input type="text" class="form-control">
+        </div>
+        <div class="col">
+            <input type="text" class="form-control">
+        </div>
+        <div class="col">
+            <input type="text" class="form-control">
+        </div>
+        <div class="col">
+            <input type="text" class="form-control">
+        </div>
+    </div>
+    <div class="form-row">
+        <div class="col">
+            <input type="text" class="form-control">
+        </div>
+        <div class="col">
+            <input type="text" class="form-control">
+        </div>
+        <div class="col">
+            <input type="text" class="form-control">
+        </div>
+        <div class="col">
+            <input type="text" class="form-control">
+        </div>
+        <div class="col">
+            <input type="text" class="form-control">
+        </div>
+    </div>
+    <div class="form-row">
+        <div class="col">
+            <input type="text" class="form-control">
+        </div>
+        <div class="col">
+            <input type="text" class="form-control">
+        </div>
+        <div class="col">
+            <input type="text" class="form-control">
+        </div>
+        <div class="col">
+            <input type="text" class="form-control">
+        </div>
+        <div class="col">
+            <input type="text" class="form-control">
+        </div>
+    </div>
+    <div class="form-row">
+        <div class="col">
+            <input type="text" class="form-control">
+        </div>
+        <div class="col">
+            <input type="text" class="form-control">
+        </div>
+        <div class="col">
+            <input type="text" class="form-control">
+        </div>
+        <div class="col">
+            <input type="text" class="form-control">
+        </div>
+        <div class="col">
+        <select class="form-control" id="exampleFormControlSelect1">
+           <!-- fill here with specific data -->
         </select>
-    </div>
-    <div class="form-group">
-        <label for="day">Odaberite dan za koji ćete praviti raspored:</label>
-        <select class="form-control" id="day" name="week_day">
-        <?php foreach($this->data['avl_days'] as $key => $day): ?>
-            <option value="<?php echo $key; ?>"><?php echo $day; ?></option>
-        <?php endforeach;?>
-        </select>
-    </div> -->
-    <div class="form-row">
-        <div class="col">
-            <input type="text" class="form-control">
-        </div>
-        <div class="col">
-            <input type="text" class="form-control">
-        </div>
-        <div class="col">
-            <input type="text" class="form-control">
-        </div>
-        <div class="col">
-            <input type="text" class="form-control">
-        </div>
-        <div class="col">
-            <input type="text" class="form-control">
         </div>
     </div>
-    <div class="form-row">
-        <div class="col">
-            <input type="text" class="form-control">
-        </div>
-        <div class="col">
-            <input type="text" class="form-control">
-        </div>
-        <div class="col">
-            <input type="text" class="form-control">
-        </div>
-        <div class="col">
-            <input type="text" class="form-control">
-        </div>
-        <div class="col">
-            <input type="text" class="form-control">
-        </div>
-    </div>
-    <div class="form-row">
-        <div class="col">
-            <input type="text" class="form-control">
-        </div>
-        <div class="col">
-            <input type="text" class="form-control">
-        </div>
-        <div class="col">
-            <input type="text" class="form-control">
-        </div>
-        <div class="col">
-            <input type="text" class="form-control">
-        </div>
-        <div class="col">
-            <input type="text" class="form-control">
-        </div>
-    </div>
-    <div class="form-row">
-        <div class="col">
-            <input type="text" class="form-control">
-        </div>
-        <div class="col">
-            <input type="text" class="form-control">
-        </div>
-        <div class="col">
-            <input type="text" class="form-control">
-        </div>
-        <div class="col">
-            <input type="text" class="form-control">
-        </div>
-        <div class="col">
-            <input type="text" class="form-control">
-        </div>
-    </div>
-    <div class="form-row">
-        <div class="col">
-            <input type="text" class="form-control">
-        </div>
-        <div class="col">
-            <input type="text" class="form-control">
-        </div>
-        <div class="col">
-            <input type="text" class="form-control">
-        </div>
-        <div class="col">
-            <input type="text" class="form-control">
-        </div>
-        <div class="col">
-            <input type="text" class="form-control">
-        </div>
-    </div>
+   
+    <input type="submit" class="btn btn-dark" value="Sačuvaj raspored!">
     </form>
 </div>
+
+<script src="http://localhost/eDiary/task1/assets/admin/js/ajax_make_sch.js"></script>

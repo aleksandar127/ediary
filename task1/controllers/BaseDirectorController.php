24bd8e68c6524f2d84c8d031d75fe85268922dd4
nenad <nenadj372@gmail.com>
2019-09-03 12:22:10 +0200

    DIrector - Chart... Controller and View

--- task1/controllers/BaseDirectorController.php
+++ task1/controllers/BaseDirectorController.php
@@ -14,5 +14,25 @@ class BaseDirectorController
 		$view->load_view('director', 'pages', 'home');
 	}
 	
-    
+   
+	public function avgclass() {
+
+		$view = new View();
+		$view->load_view('director', 'pages', 'avgclass');
+	}
+
+
+	public function avgschool() {
+
+		$view = new View();
+		$view->load_view('director', 'pages', 'avgschool');
+	}
+
+	public function logout()
+	{
+		$access_destroy = BaseAccessController::logout($_COOKIE['id'], $_COOKIE['loginhash']);
+		header('Location: '.URLROOT.'/');
+		die();
+		
+	}
 }

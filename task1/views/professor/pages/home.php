1c763a39a25e280d3967fc6ef25610183cd27353
Nenad1 <nenadj372@gmail.com>
2019-09-05 23:40:13 +0200

    Male dorade za front

--- task1/views/professor/pages/home.php
+++ task1/views/professor/pages/home.php
@@ -1,21 +1,29 @@
-<h1 class='text-center mb-4 font-weight-bold text'><?php echo $this->data['class']['name']; ?></h1>
+<div class="container-fluid">
+<h1 class="text-center mb-4 font-weight-bold text">
+	<?php echo $this->data['class']['name']; ?>
+</h1>
 
 <table class="table table-hover table-dark table-striped col-md-10 mx-auto table-home">
 <thead>
-    <th class="text-warning"><i class="fa fa-user"></i> Ime</th>
-    <th class="text-warning"><i class="fas fa-file-alt"></i> Svedocanstvo</th></thead>
+	<tr>
+    	<th class="text-warning"><i class="fa fa-user"></i> Ime</th>
+    	<th class="text-warning"><i class="fas fa-file-alt"></i> Svedocanstvo</th>
+	</tr>
+</thead>
+<tbody>
 <?php
 
 //print_r($this->data['class']);
 
 
 foreach($this->data['students'] as $students){
-    echo '<tr>';
-    echo '<td class="font-weight-bold">'.$students['last_name'].' '.$students['first_name'].'</td>';
-    echo '<td><a class="btn btn-outline-light font-weight-bold" href="'.URLROOT.'/professor/success/'.$students['id'].'">Svedocanstvo</a></td>';
-
-    echo '</tr>';
+    echo "<tr>";
+    echo '<td class="font-weight-bold col-md-6">'.$students['last_name'].' '.$students['first_name'].'</td>';
+    echo '<td class=" col-md-6"><a class="btn btn-outline-light font-weight-bold" href="'.URLROOT.'/professor/success/'.$students['id'].'">Svedocanstvo</a></td>';
+    echo "</tr>";
 
 }
 ?>
+</tbody>
 </table>
+</div>

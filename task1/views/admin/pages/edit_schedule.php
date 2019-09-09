16518a977a289cbd9b69b566d5466425e07c5902
isidora <isidoranikolic994@gmail.com>
2019-09-09 12:43:21 +0200

    sad dovuci iz baze raspored za odredjeno odeljenje ISIDORA

--- task1/views/admin/pages/edit_schedule.php
+++ task1/views/admin/pages/edit_schedule.php
@@ -1,3 +1,79 @@
 <div class="container">
-    <h1>hahaha</h1>
-</div>
+    <form method="POST" action="#">
+        <div class="form-row">
+                <div class="col">
+                    <div>Ponedeljak</div>
+                    <select class="form-control" id="monday<?php echo $this->data['counter'];?>">
+                        <!-- fill here with specific data -->
+                    </select>
+                    <small></small>
+                </div>
+                <div class="col">
+                    <div>Utorak</div>
+                    <select class="form-control" id="tuesday<?php echo $this->data['counter'];?>">
+                        <!-- fill here with specific data -->
+                    </select>
+                    <small></small>
+                </div>
+                <div class="col">
+                    <div>Sreda</div>
+                    <select class="form-control" id="wednesday<?php echo $this->data['counter'];?>">
+                        <!-- fill here with specific data -->
+                    </select>
+                    <small></small>
+                </div>
+                <div class="col">
+                    <div>Četvrtak</div>
+                    <select class="form-control" id="thursday<?php echo $this->data['counter'];?>">
+                        <!-- fill here with specific data -->
+                    </select>
+                    <small></small>
+                </div>
+                <div class="col">
+                    <div>Petak</div>
+                    <select class="form-control" id="friday<?php echo $this->data['counter'];?>">
+                        <!-- fill here with specific data -->
+                    </select>
+                    <small></small>
+                </div>
+            </div>
+
+            <?php for($i = 1; $i < 7; $i++): ?>
+            <!-- <?php //$this->data['counter']++; ?> -->
+                <div class="form-row">
+                        <div class="col">
+                            <select class="form-control" id="monday<?php echo $this->data['counter'];?>">
+                            <!-- fill here with specific data -->
+                            </select>
+                            <small></small>
+                        </div>
+                        <div class="col">
+                            <select class="form-control" id="tuesday<?php echo $this->data['counter'];?>">
+                            <!-- fill here with specific data -->
+                            </select>
+                            <small></small>
+                        </div>
+                        <div class="col">
+                            <select class="form-control" id="wednesday<?php echo $this->data['counter'];?>">
+                            <!-- fill here with specific data -->
+                            </select>
+                            <small></small>
+                        </div>
+                        <div class="col">
+                            <select class="form-control" id="thursday<?php echo $this->data['counter'];?>">
+                            <!-- fill here with specific data -->
+                            </select>
+                            <small></small>
+                        </div>
+                        <div class="col">
+                            <select class="form-control" id="friday<?php echo $this->data['counter'];?>">
+                            <!-- fill here with specific data -->
+                            </select>
+                            <small></small>
+                        </div>
+                    </div>
+                    <?php endfor; ?>
+                
+                    <input type="submit" class="btn btn-dark" value="Izmenite raspored!">
+                </div>
+        </div>

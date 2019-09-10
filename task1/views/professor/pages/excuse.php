d835de291a51f22ea701de5b84a1576338ec1757
aleksandar dmitrovic <admitrovic@yahoo.com>
2019-09-10 12:35:12 +0200

    no message

--- task1/views/professor/pages/excuse.php
+++ task1/views/professor/pages/excuse.php
@@ -1,16 +1,18 @@
 <br>
+<div style="margin-bottom:120px;">
+<h1 style="text-align:center;margin-bottom:30px;font-size:60px;">Opravdanja</h1>
 <?php
 $counter=0;
 //print_r($this->data['excuses']);
 foreach($this->data['excuses'] as $excuse):
     $counter++;
-    echo '<div class="excuseDiv" style="width:500px;object-fit:contain;">'; 
+    echo '<div class="excuseDiv" style="width:600px;object-fit:contain;">'; 
     if($counter>3){
-    echo '<img style="width:500px;" data-src="../assets/access/images/'.$excuse['image'].'"></img><figcaption>'.$excuse['last_name'].' '.$excuse['first_name'].'</figcaption><br>';
+    echo '<img style="width:600px;" data-src="../assets/access/images/'.$excuse['image'].'"></img><figcaption>'.$excuse['last_name'].' '.$excuse['first_name'].'</figcaption><br>';
     
     }
     else{
-    echo '<figure><img style=width:500px;" src="../assets/access/images/'.$excuse['image'].'" data-src="../assets/access/images/'.$excuse['image'].'"></img><figcaption>'.$excuse['last_name'].' '.$excuse['first_name'].'</figcaption></figure>';
+    echo '<figure><img style=width:600px;" src="../assets/access/images/'.$excuse['image'].'" data-src="../assets/access/images/'.$excuse['image'].'"></img><figcaption>'.$excuse['last_name'].' '.$excuse['first_name'].'</figcaption></figure>';
     }
     
     echo '</div>';
@@ -18,4 +20,5 @@ endforeach;
 
 
 ?>
+</div>
 <script src="<?php echo URLROOT; ?>/assets/professor/js/lazyload.js"></script>

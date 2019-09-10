6948ccd838935824a5f73728765b917acf5d4bbb
aleksandar dmitrovic <admitrovic@yahoo.com>
2019-09-10 10:11:49 +0200

    no message

--- task1/views/parent/pages/home.php
+++ task1/views/parent/pages/home.php
@@ -1,55 +1,28 @@
 
-<<<<<<< HEAD
+
 <div id='wrapper_home_parent'>
-=======
-<div class='container'>
-<<<<<<< HEAD
-<div class='col-md-6 text-center'>
->>>>>>> 16518a977a289cbd9b69b566d5466425e07c5902
-=======
-<div class='col-md-12 text-center bg-info p-3'>
->>>>>>> dafa74aa1913d44295c1a5ac8cd187f79b7ac37d
+
 
 <?php
 
-$sub=null;
-$name=null;
+$sub="";
+$name="";
 
 foreach($this->data['grades'] as $subject){
    
-    if($name!=$subject['first_name'])
-<<<<<<< HEAD
-<<<<<<< HEAD
+    if($name!==$subject['first_name'])
+
         echo '<br><br><h1>'.$subject['first_name'].' '.$subject['last_name'].'</h1>';
     
-    if( $sub==$subject['name'] && $name==$subject['first_name']){
+    if( $sub===$subject['name'] && $name===$subject['first_name']){
         echo "<div class='grades_home_parent'> ".$subject['grades']."</div>";
 
     }
     else
         echo "<br><div class='subject_home_parent'>".ucfirst($subject['name'])."</div><div class='grades_home_parent'> ".$subject['grades']."</div>";
-=======
-        echo '<div class="col-md-12 mb-3"><h1 class="text-center"><i class="fa fa-user"></i> '.$subject['first_name'].' '.$subject['last_name'].'</h1></div>';
-=======
-        echo '<div class="col-md-12 mb-3"><h1 class="text-center"><i class="fa fa-user text-warning"></i> '.$subject['first_name'].' '.$subject['last_name'].'</h1></div>';
->>>>>>> dafa74aa1913d44295c1a5ac8cd187f79b7ac37d
-    
-    if( $sub==$subject['name'] && $name==$subject['first_name']){
-        echo "<div style='display:inline-block;'> ".$subject['grades']." </div>";
 
-    }
-    else
-<<<<<<< HEAD
-    echo "<div class='col-md-12 text-center'>";
-        echo "<div class='col-md-6 d-inline-block font-weight-bold'>".$subject['name']."</div><div class='col-md-6 d-inline-block font-weight-bold'> ".$subject['grades']." ,</div>";
-        echo "</div>";
->>>>>>> 16518a977a289cbd9b69b566d5466425e07c5902
-=======
-    echo "<div class='row'><div class='col-md-12'>";
-        echo "<div class='col-md-4 bg-dark d-inline-block text-light font-weight-bold h4 text-center p-2 rounded mr-1'>".$subject['name']."</div>";
-        echo "<div class='col-md-4 bg-dark d-inline-block text-light font-weight-bold h4 p-2 rounded'> ".$subject['grades']." </div>";
-echo "</div></div>";
->>>>>>> dafa74aa1913d44295c1a5ac8cd187f79b7ac37d
+    
+
 
     $sub=$subject['name'];
     $name=$subject['first_name'];
@@ -57,5 +30,4 @@ echo "</div></div>";
 }
 
 ?>
-</div>
 </div>
--- task1/views/professor/pages/excuse.php
+++ task1/views/professor/pages/excuse.php
@@ -4,15 +4,15 @@ $counter=0;
 //print_r($this->data['excuses']);
 foreach($this->data['excuses'] as $excuse):
     $counter++;
-    echo '<div class="excuseDiv" style="height:300px;width:300px;object-fit:contain;">'; 
+    echo '<div class="excuseDiv" style="width:500px;object-fit:contain;">'; 
     if($counter>3){
-    echo '<img style="max-height:100%;max-width:100%;" data-src="../assets/access/images/'.$excuse['image'].'"></img><br>';
-    echo $excuse['last_name'].' '.$excuse['first_name'];
+    echo '<img style="width:500px;" data-src="../assets/access/images/'.$excuse['image'].'"></img><figcaption>'.$excuse['last_name'].' '.$excuse['first_name'].'</figcaption><br>';
+    
     }
     else{
-    echo '<img style="max-height:100%;max-width:100%;" src="../assets/access/images/'.$excuse['image'].'" data-src="../assets/access/images/'.$excuse['image'].'"></img><br>';
+    echo '<figure><img style=width:500px;" src="../assets/access/images/'.$excuse['image'].'" data-src="../assets/access/images/'.$excuse['image'].'"></img><figcaption>'.$excuse['last_name'].' '.$excuse['first_name'].'</figcaption></figure>';
     }
-    echo $excuse['last_name'].' '.$excuse['first_name'];
+    
     echo '</div>';
 endforeach;
 

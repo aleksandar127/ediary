80f203d4d386d908a87779163ab7a93eeb2ae4b6
Nenad <nenadj372@gmail.com>
2019-09-11 12:31:09 +0200

    chart

--- task1/views/director/pages/avgschool.php
+++ task1/views/director/pages/avgschool.php
@@ -43,6 +43,7 @@ categoryAxis.renderer.grid.template.location = 0;
 categoryAxis.renderer.minGridDistance = 30;
 categoryAxis.title.text = "Predmeti";
 
+
 // categoryAxis.renderer.labels.template.adapter.add("dy", function(dy, target) {
 //   if (target.dataItem && target.dataItem.index & 2 == 2) {
 //     return dy + 25;
@@ -55,6 +56,8 @@ valueAxis.min = 0;
 valueAxis.max = 5;
 valueAxis.renderer.minGridDistance = 100;
 valueAxis.title.text = "Prosek ocena";
+
+valueAxis.numberFormatter.numberFormat = "#.00";
 
 // Create series
 var series = chart.series.push(new am4charts.ColumnSeries());

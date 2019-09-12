e9ceca0e73b9ce3059209c240b8fa228c045e39e
Marjan Despotovic <makidespotovic95@gmail.com>
2019-09-12 12:24:39 +0200

    aa

--- task1/views/teacher/pages/home.php
+++ task1/views/teacher/pages/home.php
@@ -4,7 +4,7 @@
                 <h2 class="title">Ucenici</h2>
                 	<?php foreach($this->data['students'] as $student): ?>
                 <div class="student">
-					<h3 class="title"><a href="#"></span> <?php echo $student['first_name']; ?> <span> <?php echo $student['last_name'];?> </span> </a> <a class="btn btn-outline-light font-weight-bold" href="<?php echo URLROOT.'/teacher/success/'.$student['id'].' ?>">Svedocanstvo</a>'; </h3> 
+					<h3 class="title"><a href="#"></span> <?php echo $student['first_name']; ?> <span> <?php echo $student['last_name'];?> </span> </a> <a class="btn btn-outline-light font-weight-bold"; href="<?php echo URLROOT.'/teacher/success/' . $student['id'] . ?>"> Svedocanstvo </a> </h3> 
 					<?php
 					if(isset($_GET['err']) && $_GET['id']==$student['id'])
 						echo '<br><span style="color:red;font-size:20px;">'.$_GET['err'].'</span>';

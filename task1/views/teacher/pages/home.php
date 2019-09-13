008f997825f87dae1ce424b8ad41acd84fad96e6
Marjan Despotovic <makidespotovic95@gmail.com>
2019-09-13 09:23:06 +0200

    a

--- task1/views/teacher/pages/home.php
+++ task1/views/teacher/pages/home.php
@@ -6,7 +6,7 @@
 
 					<div class="student">
 						<h3 class="title"><a href="#"></span> <?php echo $student['first_name']; ?> <span> <?php echo $student['last_name']; ?></span></a> 
-						<a class="btn btn-outline-light font-weight-bold" href="<?php echo URLROOT.'/teacher/success/ . $student['id'].' ?> ">Svedocanstvo</a><?php
+						<a class="btn btn-outline-light font-weight-bold" href="<?php echo URLROOT.'/teacher/success/' . $student['id'].'" >Svedocanstvo</a>';
 						if(isset($_GET['err']) && $_GET['id']==$student['id'])
 							echo '<br><span style="color:red;font-size:20px;">'.$_GET['err'].'</span>';
 							echo '</h3>';

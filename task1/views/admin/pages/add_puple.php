b44a0b5e6f95c8fed11e469e0e8618998bd00cbd
Isidora94 <50322600+Isidora94@users.noreply.github.com>
2019-09-17 11:01:21 +0200

    pravljenje dinamicke forme za dodavanje vise ucenika u jedno odeljen je

--- task1/views/admin/pages/add_puple.php
+++ task1/views/admin/pages/add_puple.php
@@ -1,8 +1,50 @@
-<div class="container">
-<p></p>
+<!-- <div class="container">
+<p></p> -->
 <h1><?php echo $this->data['title'];?></h1>
+<a class="btn btn-success add">&#43;</a>
     <form action="<?php echo URLROOT; ?>/admin/save_new_pupils" method="POST">
+   
+    <div class="row">
+     <div class="form-group">
+            <label for="puple_n">Ime:</label>
+            <input type="text" class="form-control" id="puple_n" name="puple_n" placeholder="Petar">
+            <p></p>
+        </div>
         <div class="form-group">
+            <label for="puple_s">Prezime:</label>
+            <input type="text" class="form-control" id="puple_s" name="puple_s" placeholder="Petrović">
+            <p></p>
+        </div>
+        <div class="form-group">
+            <label for="puple_n">Ime roditelja:</label>
+            <input type="text" class="form-control" id="puple_n" name="parent_n" placeholder="Petar">
+            <p></p>
+        </div>
+        <div class="form-group">
+            <label for="puple_s">Prezime roditelja:</label>
+            <input type="text" class="form-control" id="puple_s" name="parent_s" placeholder="Petrović">
+            <p></p>
+        </div>
+        <div class="form-group">
+            <label for="parent_usr">Username roditelja:</label>
+            <input type="text" class="form-control" id="parent_usr" name="parent_username" placeholder="mikica">
+            <p></p>
+        </div>
+
+        <div class="form-group">
+            <label for="pass">Generisati šifru roditelja:</label>
+            <input type="text" class="form-control" id="pass" name="parent_pass" placeholder="123456">
+            <p></p>
+        </div>
+
+        <div class="form-group">
+            <label for="re_pass">Potvrditi šifru:</label>
+            <input type="text" class="form-control" id="re_pass" name="parent_re_pass" placeholder="123456">
+            <p></p>
+    </div>
+   
+  </div>
+        <!-- <div class="form-group">
             <label for="puple_n">Ime:</label>
             <input type="text" class="form-control" id="puple_n" name="puple_n" placeholder="Petar">
             <p></p>
@@ -38,12 +80,11 @@
             <label for="re_pass">Potvrditi šifru:</label>
             <input type="text" class="form-control" id="re_pass" name="parent_re_pass" placeholder="123456">
             <p></p>
-        </div>
-        <a class="btn btn-success add" style="width:40px; color:#fff;">&#43;</a>
+        </div> -->
 
         <button type="submit" class="btn btn-dark">Dodaj učenika</button>
     </form>
 
-</div>
+<!-- </div> -->
 
 <script src="<?php echo URLROOT; ?>/assets/admin/js/add_pupils.js"></script>

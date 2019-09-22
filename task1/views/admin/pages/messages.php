1b9f49bbf5451d39496b1f03a67371f614c08cf2
Isidora94 <50322600+Isidora94@users.noreply.github.com>
2019-09-22 20:06:20 +0200

    ll

--- task1/views/admin/pages/messages.php
+++ task1/views/admin/pages/messages.php
@@ -1,3 +1,10 @@
 <div class="container">
+    <form action="#" method="POST">
+        <div class="form-group">
+            <label for="parent_news">Ispiši novo obaveštenje za roditelje:</label>
+            <textarea class="form-control" id="parent_news" name="parent_news" rows="3"></textarea>
+        </div>
+        <input type="submit" class="btn btn-dark" value="Ispiši">
+    </form>
     
 </div>

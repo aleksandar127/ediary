cb9c12cefff5e293858e441a429ea0e241af79af
isidora <isidoranikolic994@gmail.com>
2019-09-23 11:19:44 +0200

    omgggg

--- task1/views/admin/pages/add_puple.php
+++ task1/views/admin/pages/add_puple.php
@@ -2,44 +2,44 @@
 <h1><?php echo $this->data['title'];?></h1>
 <a class="btn btn-success add">&#43;</a>
 
-    <form action="<?php echo URLROOT; ?>/admin/save_new_pupils" method="POST">
-   
+    <form action="<?php echo URLROOT; ?>/admin/save_new_pupils/<?php echo $this->data['class_id'];?>" method="POST">
+    
     <div class="row">
      <div class="form-group">
             <label for="puple_n">Ime:</label>
-            <input type="text" class="form-control" id="puple_n" name="puple_n" placeholder="Petar">
+            <input type="text" class="form-control" id="puple_n" name="puple_n1" placeholder="Petar">
             <p></p>
         </div>
         <div class="form-group">
             <label for="puple_s">Prezime:</label>
-            <input type="text" class="form-control" id="puple_s" name="puple_s" placeholder="Petrović">
+            <input type="text" class="form-control" id="puple_s" name="puple_s1" placeholder="Petrović">
             <p></p>
         </div>
         <div class="form-group">
             <label for="puple_n">Ime roditelja:</label>
-            <input type="text" class="form-control" id="parent_n" name="parent_n" placeholder="Petar">
+            <input type="text" class="form-control" id="parent_n" name="parent_n1" placeholder="Petar">
             <p></p>
         </div>
         <div class="form-group">
             <label for="puple_s">Prezime roditelja:</label>
-            <input type="text" class="form-control" id="parent_s" name="parent_s" placeholder="Petrović">
+            <input type="text" class="form-control" id="parent_s" name="parent_s1" placeholder="Petrović">
             <p></p>
         </div>
         <div class="form-group">
             <label for="parent_usr">Username roditelja:</label>
-            <input type="text" class="form-control" id="parent_usr" name="parent_username" placeholder="mikica">
+            <input type="text" class="form-control" id="parent_usr" name="parent_username1" placeholder="mikica">
             <p></p>
         </div>
 
         <div class="form-group">
             <label for="pass">Generisati šifru roditelja:</label>
-            <input type="text" class="form-control" id="pass" name="parent_pass" placeholder="123456">
+            <input type="text" class="form-control" id="pass" name="parent_pass1" placeholder="123456">
             <p></p>
         </div>
 
         <div class="form-group">
             <label for="re_pass">Potvrditi šifru:</label>
-            <input type="text" class="form-control" id="re_pass" name="parent_re_pass" placeholder="123456">
+            <input type="text" class="form-control" id="re_pass" name="parent_re_pass1" placeholder="123456">
             <p></p>
     </div>
    

f9884a70222d04b0fb4353280ed0f6f52373d34f
aleksandar dmitrovic <admitrovic@yahoo.com>
2019-09-25 12:12:21 +0200

    no message

--- task1/views/professor/pages/home.php
+++ task1/views/professor/pages/home.php
@@ -8,7 +8,7 @@
 		<thead>
 			<tr>
 				<th class="text-warning text-center h4 font-weight-bold"><i class="fa fa-user"></i> Ime</th>
-				<th class="text-warning text-center h4 font-weight-bold"><i class="fas fa-file-alt"></i> Svedocanstvo</th>
+				<th class="text-warning text-center h4 font-weight-bold"><i class="fas fa-file-alt"></i> Svedočanstvo</th>
 			</tr>
 		</thead>
 		<tbody>
@@ -18,7 +18,7 @@
 			foreach($this->data['students'] as $students){
 				echo "<tr>";
 				echo '<td class="text-center font-weight-bold">'.ucfirst($students['last_name']).' '.ucfirst($students['first_name']).'</td>';
-				echo '<td class="text-center"><a class="btn btn-outline-light font-weight-bold" href="'.URLROOT.'/professor/success/'.$students['id'].'">Svedocanstvo</a>';
+				echo '<td class="text-center"><a class="btn btn-outline-light font-weight-bold" href="'.URLROOT.'/professor/success/'.$students['id'].'">Svedočanstvo</a>';
 				
 				if(isset($_GET['err']) && $_GET['id']==$students['id'])
 					echo '<br><span style="color:red;">'.$_GET['err'].'</span>';

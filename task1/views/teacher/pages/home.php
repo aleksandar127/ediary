0eb263848f8da25f1261219da4006fa096d8f90e
Marjan Despotovic <makidespotovic95@gmail.com>
2019-10-01 11:18:30 +0200

    no message

--- task1/views/teacher/pages/home.php
+++ task1/views/teacher/pages/home.php
@@ -14,19 +14,17 @@
 				<?php foreach($this->data['students'] as $student): ?>
 					<tr>
 						<td class="name_student" class="text-center font-weight-bold"><span><?php echo $student['first_name']; ?></span> <span><?php echo $student['last_name']; ?></span></td>
-						<td class="text-center"><a id="testimony" onclick="myFunction()" class="btn btn-outline-light font-weight-bold" href="<?php echo URLROOT.'/teacher/success/' . $student['id'].''?>" target="_blank">Svedocanstvo</a>
+						<td class="text-center"><a id="testimony" onclick="myFunction()" class="btn btn-outline-light font-weight-bold" href="<?php echo URLROOT.'/teacher/success/' . $student['id'].''?>" >Svedocanstvo</a>
 						
-						<?php if(isset($_GET['err']) && $_GET['id'] == $student['id']){ ?> 
+						<script>
+							function myFunction() {
+								if($_GET['err']){
+									document.getElementById("testimony").setAttribute("target", "_self");
+								}
+							}
+						</script>
 
-<script>
-function myFunction() {
-	  var testimony_id = document.getElementById("testimony");
-	  var testimony_attribute = document.createAttribute("target");
-	  testimony_attribute.value = "_self"
-	  testimony_id.setAttributeNode(testimony_attribute); 
-}
-</script>
-<?php
+						<?php if(isset($_GET['err']) && $_GET['id'] == $student['id']){ 
 							echo '<br><span style="color:red;font-size:20px;">'.$_GET['err'].'</span>';
 						}?></td>
 					</tr>
